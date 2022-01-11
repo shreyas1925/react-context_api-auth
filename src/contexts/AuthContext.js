fd90eedd7c08f4da23b7d20af957aed4cc6f6b1e
Shreyas <70105807+shreyas1925@users.noreply.github.com>
2022-01-11 23:18:56 +0530

    Update AuthContext.js

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,8 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import { auth } from "../firebase";
-
 const AuthContext = React.createContext();
-
 export function useAuth() {
   return useContext(AuthContext);
 }
